de5f6f6490f3c9bd4ace832c29a8e1f44426989e
Ryanfzhang <mafzhang@ust.hk>
2024-08-29 02:49:02 +0800

    add chesapeake

--- script/train_trmf.sh
+++ script/train_trmf.sh
@@ -1,5 +1,5 @@
-python imputation/train_trmf.py --missing_ratio 0.1
-python imputation/train_trmf.py --missing_ratio 0.3
-python imputation/train_trmf.py --missing_ratio 0.5
-python imputation/train_trmf.py --missing_ratio 0.7
-python imputation/train_trmf.py --missing_ratio 0.9
+python imputation/train_trmf.py --missing_ratio 0.1 --area Chesapeake 
+python imputation/train_trmf.py --missing_ratio 0.3 --area Chesapeake 
+python imputation/train_trmf.py --missing_ratio 0.5 --area Chesapeake 
+python imputation/train_trmf.py --missing_ratio 0.7 --area Chesapeake 
+python imputation/train_trmf.py --missing_ratio 0.9 --area Chesapeake 
