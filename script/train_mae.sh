c594c514ebe10965a35e5a86a53ed1b314b91e04
Ryanfzhang <mafzhang@ust.hk>
2024-08-29 15:31:17 +0800

    receive args via bash

--- script/train_mae.sh
+++ script/train_mae.sh
@@ -1,5 +1,5 @@
-python imputation/train_mae.py --missing_ratio 0.9 --area Chesapeake 
-python imputation/train_mae.py --missing_ratio 0.7 --area Chesapeake 
-python imputation/train_mae.py --missing_ratio 0.5 --area Chesapeake 
-python imputation/train_mae.py --missing_ratio 0.3 --area Chesapeake 
-python imputation/train_mae.py --missing_ratio 0.1 --area Chesapeake 
+python imputation/train_mae.py --missing_ratio 0.9 --area $0 
+python imputation/train_mae.py --missing_ratio 0.7 --area $0 
+python imputation/train_mae.py --missing_ratio 0.5 --area $0 
+python imputation/train_mae.py --missing_ratio 0.3 --area $0 
+python imputation/train_mae.py --missing_ratio 0.1 --area $0 
