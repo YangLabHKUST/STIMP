97348b8e2a6c97ceed55f219b624263d2a78a8b3
Ryanfzhang <mafzhang@ust.hk>
2024-08-31 15:44:24 +0800

    add Chesapeake and Yangtze

--- prediction/train_xgboost_without_imputation.py
+++ prediction/train_xgboost_without_imputation.py
@@ -40,8 +40,12 @@ config = parser.parse_args()
 
 if config.area=="MEXICO":
     config.height, config.width = 36, 120
-elif args.area=="PRE":
+elif config.area=="PRE":
     config.height, config.width = 60, 96
+elif config.area=="Chesapeake":
+    config.height, config.width = 60, 48
+elif config.area=="Yangtze":
+    config.height, config.width = 96, 72
 else:
     print("Not Implement")
 
