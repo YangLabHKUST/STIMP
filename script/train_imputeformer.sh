956aab6016b8f202c292a49d77c9f72e8fa90a3f
Ryanfzhang <mafzhang@ust.hk>
2024-10-23 10:21:14 +0800

    new script

--- script/train_imputeformer.sh
+++ script/train_imputeformer.sh
@@ -1,5 +1,4 @@
-python imputation/train_imputeformer.py --missing_ratio 0.9 --area $1 
-python imputation/train_imputeformer.py --missing_ratio 0.7 --area $1 
-python imputation/train_imputeformer.py --missing_ratio 0.5 --area $1 
-python imputation/train_imputeformer.py --missing_ratio 0.3 --area $1 
-python imputation/train_imputeformer.py --missing_ratio 0.1 --area $1 
+python imputation/train_imputeformer.py --missing_ratio 0.2 --area $1 
+python imputation/train_imputeformer.py --missing_ratio 0.4 --area $1 
+python imputation/train_imputeformer.py --missing_ratio 0.6 --area $1 
+python imputation/train_imputeformer.py --missing_ratio 0.8 --area $1 
