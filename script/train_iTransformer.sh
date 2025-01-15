e44da9769b630e7888373ae3950ec2c273a48d1c
Fan Zhang <mafzhang@ust.hk>
2025-01-15 16:11:39 +0800

    Update train_iTransformer.sh

--- script/train_iTransformer.sh
+++ script/train_iTransformer.sh
@@ -1,5 +1,5 @@
-# for i in {0..9}  
-# do  
-# python prediction/train_without_spatial.py --index $i --method "iTransformer" --area $1
-# done
+for i in {0..9}  
+do  
+python prediction/train_without_spatial.py --index $i --method "iTransformer" --area $1
+done
 python prediction/train_without_spatial_imputation.py  --method "iTransformer" --area $1
