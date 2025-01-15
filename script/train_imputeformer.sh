11205cd0dc16d77dd5371ba5ea52766eb259a73a
Fan Zhang <mafzhang@ust.hk>
2025-01-15 16:12:27 +0800

    Update train_imputeformer.sh

--- script/train_imputeformer.sh
+++ script/train_imputeformer.sh
@@ -1,4 +1,4 @@
-python imputation/train_imputeformer.py --missing_ratio 0.2 --area $1 
-python imputation/train_imputeformer.py --missing_ratio 0.4 --area $1 
-python imputation/train_imputeformer.py --missing_ratio 0.6 --area $1 
-python imputation/train_imputeformer.py --missing_ratio 0.8 --area $1 
+for i in {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9}
+do
+  python imputation/train_imputeformer.py --missing_ratio $i --area $1
+done
