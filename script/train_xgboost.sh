9fe0934048b2240a3a4d623a139f8f0befcf575e
Fan Zhang <mafzhang@ust.hk>
2025-01-15 15:59:20 +0800

    Update train_xgboost.sh

--- script/train_xgboost.sh
+++ script/train_xgboost.sh
@@ -2,5 +2,3 @@ for i in {0..9}
 do  
 python prediction/train_xgboost.py --index $i --area $1
 done
-python tmp/merge_predictions.py --area $1
-# python prediction/train_xgboost_without_imputation.py --area $1
