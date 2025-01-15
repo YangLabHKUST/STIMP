f14c5131212fc92fda20daa06a84c8bbe10c97a7
Fan Zhang <mafzhang@ust.hk>
2025-01-15 15:45:33 +0800

    Update train_stimp_wotemporal.py

--- imputation/train_stimp_wotemporal.py
+++ imputation/train_stimp_wotemporal.py
@@ -12,7 +12,7 @@ import sys
 sys.path.insert(0, os.getcwd())
 from dataset.dataset_imputation import PRE8dDataset
 from utils import check_dir, masked_mae, masked_mse, seed_everything
-from model.graphdiffusion_wotemporal import IAP_base
+from model.stimp_wotemporal import IAP_base
 
 parser = argparse.ArgumentParser(description='Imputation')
 
