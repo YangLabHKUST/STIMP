e8f4b357119228fa62b4814838515c959f220eb5
Fan Zhang <mafzhang@ust.hk>
2025-01-15 16:12:46 +0800

    Update train_inpainter.sh

--- script/train_inpainter.sh
+++ script/train_inpainter.sh
@@ -3,3 +3,7 @@ python imputation/train_inpainter.py --missing_ratio 0.3 --area $1
 python imputation/train_inpainter.py --missing_ratio 0.5 --area $1 
 python imputation/train_inpainter.py --missing_ratio 0.7 --area $1 
 python imputation/train_inpainter.py --missing_ratio 0.9 --area $1 
+for i in {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9}
+do
+  python imputation/train_inpainter.py --missing_ratio $i --area $1
+done
