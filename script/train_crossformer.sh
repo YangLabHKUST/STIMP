7ffcc7a298f098a79f32b22ec2868106dfba145a
Fan Zhang <mafzhang@ust.hk>
2025-01-15 15:57:34 +0800

    Update train_crossformer.sh

--- script/train_crossformer.sh
+++ script/train_crossformer.sh
@@ -1,5 +1,5 @@
-# for i in {0..9}  
-# do  
-# python prediction/train_without_spatial.py --index $i --method "CrossFormer" --area $1
-# done
-python prediction/train_without_spatial_imputation.py  --method "CrossFormer" --area $1
+for i in {0..9}  
+do  
+  python prediction/train_without_spatial.py --index $i --method "CrossFormer" --area $1
+done
+
