d574714c20909779747e80dbbcf66924a016a774
Fan Zhang <mafzhang@ust.hk>
2025-01-15 16:10:52 +0800

    Update and rename train_graphdiffusion_wotemporal.sh to train_stimp_wotemporal.sh

--- script/train_graphdiffusion_wotemporal.sh
+++
@@ -1,5 +0,0 @@
-python imputation/train_graphdiffusion_wotemporal.py --missing_ratio 0.1 --area $1 
-python imputation/train_graphdiffusion_wotemporal.py --missing_ratio 0.3 --area $1 
-python imputation/train_graphdiffusion_wotemporal.py --missing_ratio 0.5 --area $1 
-python imputation/train_graphdiffusion_wotemporal.py --missing_ratio 0.7 --area $1 
-python imputation/train_graphdiffusion_wotemporal.py --missing_ratio 0.9 --area $1 
