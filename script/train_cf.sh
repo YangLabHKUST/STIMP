a1cb94c8578dcea7ed9ff4eca340abafd39a741f
Fan Zhang <mafzhang@ust.hk>
2025-01-15 16:04:29 +0800

    Update train_cf.sh

--- script/train_cf.sh
+++ script/train_cf.sh
@@ -3,3 +3,7 @@ python imputation/train_cf.py --missing_ratio 0.3 --area $1
 python imputation/train_cf.py --missing_ratio 0.5 --area $1 
 python imputation/train_cf.py --missing_ratio 0.7 --area $1 
 python imputation/train_cf.py --missing_ratio 0.9 --area $1 
+python imputation/train_cf.py --missing_ratio 0.2 --area $1 
+python imputation/train_cf.py --missing_ratio 0.4--area $1 
+python imputation/train_cf.py --missing_ratio 0.6 --area $1 
+python imputation/train_cf.py --missing_ratio 0.8 --area $1 
